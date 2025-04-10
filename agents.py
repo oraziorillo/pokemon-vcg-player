257318394b21aee96952498b4353ef6fdbfe1d42
Joffrey THOMAS <Jofthomas@users.noreply.huggingface.co>
2025-04-10 14:48:48 +0000

    Update agents.py

--- agents.py
+++ agents.py
@@ -10,7 +10,6 @@ from poke_env.player import Player
 from poke_env.environment.battle import Battle
 from poke_env.environment.move import Move
 from poke_env.environment.pokemon import Pokemon
-from poke_env.player import Observation # Observation might not be directly used here, but good to keep if extending
 
 
 class OpenAIAgent(Player):
